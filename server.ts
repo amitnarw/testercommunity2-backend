906bc6fe41932554b9c91db729fa632e9f712cb2
dev <dsbdev08@gmail.com>
2025-11-11 18:17:57 +0530

    Added database schema

--- server.ts
+++ server.ts
@@ -22,28 +22,28 @@ app.use(cors(allowedOrigins));
 
 app.all("/api/auth/*", toNodeHandler(auth));
 
-app.get("/api/profile", async (req, res) => {
-  const session = await auth.api.getSession({
-    headers: fromNodeHeaders(req.headers),
-  });
+// app.get("/api/profile", async (req, res) => {
+//   const session = await auth.api.getSession({
+//     headers: fromNodeHeaders(req.headers),
+//   });
 
-  if (!session) {
-    return res.status(401).json({ error: "Not logged in" });
-  }
+//   if (!session) {
+//     return res.status(401).json({ error: "Not logged in" });
+//   }
 
-  res.json({ user: session.user });
-});
+//   res.json({ user: session.user });
+// });
 
-app.get("/api/admin", async (req, res) => {
-  const session = await auth.api.getSession({
-    headers: fromNodeHeaders(req.headers as any),
-  });
-  if (!session || session.user.role !== "admin") {
-    return res.status(403).json({ error: "Forbidden" });
-  }
+// app.get("/api/admin", async (req, res) => {
+//   const session = await auth.api.getSession({
+//     headers: fromNodeHeaders(req.headers as any),
+//   });
+//   if (!session || session.user.role !== "admin") {
+//     return res.status(403).json({ error: "Forbidden" });
+//   }
 
-  res.json({ message: `Welcome Admin ${session.user.email}` });
-});
+//   res.json({ message: `Welcome Admin ${session.user.email}` });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
