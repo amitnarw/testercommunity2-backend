56621f47aa4479729098a75b4d36e63fd076e82a
dev <dsbdev08@gmail.com>
2025-11-14 18:29:39 +0530

    User details schema added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,53 @@
-const register = () => {
-    try {
-        
-    } catch (err) {
-        
+import { type Request, type Response } from "express";
+import { sendError, sendSuccess } from "@/utils/response";
+import { prismaClient } from "@/lib/prisma";
+
+const register = async (req: Request, res: Response) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      phone,
+      user_type,
+      job_role,
+      company: {
+        company_name,
+        company_size,
+        position_in_company,
+        company_website,
+      },
+      experience: {
+        experience_level,
+        total_published_apps,
+        platform_development,
+        publish_frequency,
+      },
+      service_usage,
+      communication: { preferred_methods, notifications },
+    } = req.body;
+
+    if (!first_name || !last_name || !email || !password || !user_type) {
+      return sendError(
+        res,
+        404,
+        "First name, last name, email, password and user type are required"
+      );
     }
-}
+
+    const response = await prismaClient?.user?.create({
+          first_name,
+          last_name,
+          email,
+          
+    })
+    return sendSuccess(res, { payload: "Registered successfully" }, "ok");
+  } catch (error) {
+    return sendError(
+      res,
+      400,
+      error instanceof Error ? error.message : "Unknown error"
+    );
+  }
+};
