1ec11b606a80a425b72f48d77649320adfedd95d
dev <dsbdev08@gmail.com>
2025-12-04 13:34:45 +0530

    added role functionality and other fields

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,30 @@
-import { betterAuth } from "better-auth";
+import { APIError, betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prismaClient } from "./prisma";
 import { sendEmail } from "@/services/resend";
+import { customSession } from "better-auth/plugins";
+import type { UserAuthType } from "prisma/generated/prisma";
+
+const rolePlugin = customSession(async ({ user, session }) => {
+  const role = await prismaClient?.userDetail?.findUnique({
+    where: {
+      userId: session?.userId,
+    },
+    select: {
+      role: {
+        select: {
+          name: true,
+          permissions: true,
+        },
+      },
+    },
+  });
+  return {
+    role,
+    user,
+    session,
+  };
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prismaClient, {
@@ -15,7 +38,12 @@ export const auth = betterAuth({
 
   session: {
     expiresIn: 60 * 60 * 24 * 7,
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds (5 minutes)
+    },
   },
+  plugins: [rolePlugin],
 
   trustedOrigins: [process.env.CORS_ORIGIN ?? "http://localhost:3000"],
 
@@ -35,6 +63,64 @@ export const auth = betterAuth({
     async afterEmailVerification(user, request) {
       // Your custom logic here, e.g., grant access to premium features
       console.log(`${user.email} has been successfully verified!`);
+    },
+  },
+
+  databaseHooks: {
+    user: {
+      create: {
+        before: async (data, ctx) => {
+          const role = ctx?.body.role as string | undefined;
+
+          if (!role) {
+            throw new APIError("BAD_REQUEST", {
+              message: "Role is required",
+            });
+          }
+
+          const roleRecord = await prismaClient?.role.findFirst({
+            where: { name: role },
+          });
+
+          if (!roleRecord) {
+            throw new APIError("BAD_REQUEST", {
+              message: "Role name not found",
+            });
+          }
+
+          const { role: _discard, ...cleanData } = data;
+          (ctx as any).state = { resolvedRole: roleRecord };
+          return { data: cleanData };
+        },
+
+        after: async (user, ctx) => {
+          const roleRecord = (ctx as any).state.resolvedRole as {
+            id: number;
+            createdAt: Date;
+            updatedAt: Date;
+            name: string;
+          };
+          const authType = (ctx?.body?.authType as string) ?? "user";
+          const firstName = ctx?.body?.firstName as string;
+          const lastName = ctx?.body?.lastName as string;
+
+          await prismaClient?.userDetail?.upsert({
+            where: {
+              userId: user?.id,
+            },
+            update: {
+              roleId: roleRecord?.id,
+            },
+            create: {
+              userId: user?.id,
+              first_name: firstName,
+              last_name: lastName,
+              authType: authType as UserAuthType,
+              roleId: roleRecord?.id,
+            },
+          });
+        },
+      },
     },
   },
 });
