26fefe2b93d109aaaa3ffe6e68be55980112b9fa
dev <dsbdev08@gmail.com>
2026-01-17 19:47:05 +0530

    prisma build issues fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "../../prisma/generated/prisma";
+// import { PrismaClient } from "@@/prisma/generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 // const globalForPrisma = global as unknown as { prisma: PrismaClient };
