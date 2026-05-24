70dda309b80617be08a77780d85978d3f31909a6
Amit <amitnarwal115@gmail.com>
2026-05-24 12:01:21 +0530

    readded the problamatic columns

---
+++ prisma/migrations/20260524063018_new_migratopn_v2/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "user_detail" ADD COLUMN     "tester_devices" TEXT[] DEFAULT ARRAY[]::TEXT[],
+ADD COLUMN     "tester_os_versions" TEXT[] DEFAULT ARRAY[]::TEXT[],
+ADD COLUMN     "testing_types" TEXT[] DEFAULT ARRAY[]::TEXT[];
